25d5be1845e2344ca729883c1a27c7da36c7fcbd
Tiago Carmo <tiago@Tiagos-MacBook-Pro.local>
2023-02-17 20:36:43 -0300

    add gem pry and rspec and user migration

---
+++ db/migrate/20230217230824_add_user.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# app/model/users
+class AddUser < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :name, :cpf, :phone, :password_digest
+      t.string :email, index: { unique: true, name: 'unique_emails' }
+      t.boolean :admin, null: false, default: false
+      t.boolean :is_active, default: true
+
+      t.timestamps
+    end
+  end
+end
